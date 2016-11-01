76d727acc1ca3058914d05cb9ee6b3a6400d17ab
naveedzafar-unstoppable <naveed.zafar@unstoppable.io>
2016-11-02 04:46:22 +0500

    api to db
    saving api information to db apps

--- Controllers/SalesForceController.cs
+++ Controllers/SalesForceController.cs
@@ -1,14 +1,15 @@
 ﻿using Salesforce.Common;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Salesforce.Common.Models;
 using Salesforce.Force;
-using Newtonsoft.Json;
 using System.Configuration;
+using Newtonsoft.Json;
+using MySql.Data.MySqlClient;
+using System.Text;
 
 namespace SalesForceOAuth.Controllers
 {
@@ -21,122 +22,253 @@ namespace SalesForceOAuth.Controllers
     public class SalesForceController : ApiController
     {
         /// <summary>
-        /// GET: api/SalesForce
-        /// </summary>
-        /// <returns>string array saying API head line</returns>
-        public IEnumerable<string> GetApiInfo()
-        {
-            return new string[] { "This Api is meant to work with website alive chat application." };
-        }
-
-
-        /// <summary>
-        /// GET: api/SalesForce/GetRedirectURL?ValidationKey= ffe06298-22a8-4849-a46c-0284b04f2561
+        /// GET: api/SalesForce/GetRedirectURL
+        /// ValidationKey in header
         /// This is the first step of OAuth, getting the URL to redirect to. 
         /// </summary>
         /// <param name="ValidationKey"></param>
         /// <returns></returns>
         [HttpGet]
         [ActionName("GetRedirectURL")]
-        public string GetRedirectURL(string ValidationKey)
+        public HttpResponseMessage GetRedirectURL()
         {
-            if (ValidationKey == ConfigurationManager.AppSettings["APISecureKey"])
-            { 
-                // Response.Write("started TEsting");
-                var url =
-                Common.FormatAuthUrl(
-                    "https://login.salesforce.com/services/oauth2/authorize",
-                    ResponseTypes.Code,
-                    "3MVG9KI2HHAq33RwXJsqtsEtY.ThMCzS5yZd3S8CzXBArijS0WEQgYACVnQ9SJq0KDdKrQgIxPFNPOIQhuqdK",
-                    System.Web.HttpUtility.UrlEncode("http://localhost:56786/About.aspx"));
-                return url;
+            string ValidationKey = "", sf_authoize_url = "", sf_clientid = "", sf_callback_url=""; 
+            HttpResponseMessage outputResponse = new HttpResponseMessage();
+            var re = Request;
+            var headers = re.Headers;
+            if (headers.Contains("ValidationKey"))
+            {
+                ValidationKey = HttpRequestMessageExtensions.GetHeader(re, "ValidationKey");
+                MyAppsDb.GetRedirectURLParameters(ref sf_authoize_url, ref sf_clientid, ref sf_callback_url); 
             }
-            return "Error: Authenticating App"; 
-            
-        }
+            else
+            {
+                outputResponse.StatusCode = HttpStatusCode.Unauthorized;
+                outputResponse.Content = new StringContent("Your request isn't authorized!");
+                return outputResponse;
+            }
 
-        // GET: api/SalesForce/GetAuthorizationToken?ValidationKey= ffe06298-22a8-4849-a46c-0284b04f2561
-        [HttpGet]
-        [ActionName("GetAuthorizationToken")]
-        public string GetAuthorizationToken(string AuthCode, string ValidationKey)
-        {
             if (ValidationKey == ConfigurationManager.AppSettings["APISecureKey"])
             {
-                string _consumerKey = "3MVG9KI2HHAq33RwXJsqtsEtY.ThMCzS5yZd3S8CzXBArijS0WEQgYACVnQ9SJq0KDdKrQgIxPFNPOIQhuqdK";
-                string _consumerSecret = "7849687416745281703";
-                string _callbackUrl = "http://localhost:56786/About.aspx";
-                string _tokenRequestEndpointUrl = "https://login.salesforce.com/services/oauth2/token";
-                string code = AuthCode;
+                // Response.Write("started TEsting");
+                //var url =
+                //Common.FormatAuthUrl(
+                //    "https://login.salesforce.com/services/oauth2/authorize",
+                //    ResponseTypes.Code,
+                //    "3MVG9KI2HHAq33RwXJsqtsEtY.ThMCzS5yZd3S8CzXBArijS0WEQgYACVnQ9SJq0KDdKrQgIxPFNPOIQhuqdK",
+                //    System.Web.HttpUtility.UrlEncode("http://localhost:56786/About.aspx"));
+
+                var url = Common.FormatAuthUrl(sf_authoize_url, ResponseTypes.Code, sf_clientid, System.Web.HttpUtility.UrlEncode(sf_callback_url));
+                outputResponse.StatusCode = HttpStatusCode.OK;
+                outputResponse.Content = new StringContent(url.ToString(), Encoding.UTF8, "application/json");
+                return outputResponse;
+            }
+            else
+            {
+                outputResponse.StatusCode = HttpStatusCode.Unauthorized;
+                outputResponse.Content = new StringContent("Your request isn't authorized!");
+                return outputResponse;
+            }
+
+}
+
+        //GET: api/SalesForce/GetAuthorizationToken? ValidationKey = ffe06298 - 22a8-4849-a46c-0284b04f2561
+        [HttpGet]
+        [ActionName("GetAuthorizationToken")]
+        public HttpResponseMessage GetAuthorizationToken(string IsNew)
+        {
+            string ValidationKey = "", AuthCode = "", username = "", sf_clientid = "", sf_callback_url = "", sf_consumer_key = "", sf_consumer_secret = "", sf_token_req_end_point = "";
+            HttpResponseMessage outputResponse = new HttpResponseMessage();
+            var re = Request;
+            var headers = re.Headers;
+            if (headers.Contains("ValidationKey") && headers.Contains("AuthCode") && headers.Contains("username"))
+            {
+                ValidationKey = HttpRequestMessageExtensions.GetHeader(re, "ValidationKey");
+                AuthCode = HttpRequestMessageExtensions.GetHeader(re, "AuthCode");
+                username = HttpRequestMessageExtensions.GetHeader(re, "username");
+                MyAppsDb.GetTokenParameters(ref sf_clientid, ref sf_callback_url, ref sf_consumer_key, ref sf_consumer_secret, ref sf_token_req_end_point);
+            }
+            else
+            {
+                outputResponse.StatusCode = HttpStatusCode.Unauthorized;
+                outputResponse.Content = new StringContent("Your request isn't authorized!");
+                return outputResponse;
+            }
+            if (ValidationKey == ConfigurationManager.AppSettings["APISecureKey"])
+            {
                 ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
                 var auth = new AuthenticationClient();
                 try
                 {
-                    auth.WebServerAsync(_consumerKey, _consumerSecret, _callbackUrl, code, _tokenRequestEndpointUrl).Wait();
-                    
-                }
-                catch(Exception ex)
-                {
-                    return "Error authenticating" + ex.InnerException; 
-                }
-                var url = string.Format("/?token={0}&api={1}&instance_url={2}&refresh_token={3}",
-                    auth.AccessToken,
-                    auth.ApiVersion,
-                    auth.InstanceUrl,
-                    auth.RefreshToken);
-                var response = new HttpResponseMessage(HttpStatusCode.Redirect);
-                response.Headers.Location = new Uri(url, UriKind.Relative);
-
-                string output = string.Format("token={0}&api={1}&instance_url={2}",
-                    auth.AccessToken,
-                    auth.ApiVersion,
-                    auth.InstanceUrl
-                    ); 
-                return output; 
-            }
-            return "Error: Authenticating App"; 
-        }
-
-        // GET: api/SalesForce/GetLeadToken?ValidationKey= ffe06298-22a8-4849-a46c-0284b04f2561
-        [HttpGet]
-        [ActionName("GetTotalLeads")]
-        public async System.Threading.Tasks.Task<List<Lead>> GetAllLeads(string AccessToken, string InstanceUrl, string ApiVersion, string ValidationKey)
-        {
-            if (ValidationKey == ConfigurationManager.AppSettings["APISecureKey"])
-            {
-                List<Lead> myLeads = new List<Lead> { };
-                try
-                {
-                    ForceClient client = new ForceClient(InstanceUrl, AccessToken, ApiVersion);
-                    QueryResult<dynamic> cont = await client.QueryAsync<dynamic>("SELECT Id, FirstName, LastName, Company From Lead");
-                    foreach (dynamic c in cont.Records)
+                    if (IsNew.Equals("Y"))
                     {
-                        Lead l = new Lead();
-                        l.Id = c.Id;
-                        l.FirstName = c.FirstName;
-                        l.LastName = c.LastName;
-                        l.Company = c.Company;
-                        myLeads.Add(l); 
+                        auth.WebServerAsync(sf_consumer_key, sf_consumer_secret, sf_callback_url, AuthCode, sf_token_req_end_point).Wait();
+                        MyAppsDb.CreateNewIntegrationSettingForUser(username, auth.RefreshToken, auth.AccessToken, auth.ApiVersion, auth.InstanceUrl);
                     }
-                    return myLeads; 
+                    else
+                    {
+                        string SFRefreshToken="";
+                        MyAppsDb.GetCurrentRefreshToken(username, ref SFRefreshToken);
+                        auth.TokenRefreshAsync(sf_clientid, SFRefreshToken, sf_consumer_secret, sf_token_req_end_point).Wait();
+                        MyAppsDb.UpdateIntegrationSettingForUser(username, auth.AccessToken, auth.ApiVersion, auth.InstanceUrl); 
+                    }
+                    
+                    outputResponse.StatusCode = HttpStatusCode.OK;
+                    outputResponse.Content = new StringContent("API information updated!");
+                    return outputResponse;
                 }
                 catch (Exception ex)
                 {
-                    return myLeads;
+                    outputResponse.StatusCode = HttpStatusCode.InternalServerError;
+                    outputResponse.Content = new StringContent("API error:" + ex.InnerException);
+                    return outputResponse;
                 }
+                /**
+
+                var response = new HttpResponseMessage(HttpStatusCode.Redirect);
+                response.Headers.Location = new Uri(url, UriKind.Relative);*/
             }
-            return null; // if not authenticated 
+            else
+            {
+                outputResponse.StatusCode = HttpStatusCode.Unauthorized;
+                outputResponse.Content = new StringContent("Your request isn't authorized!");
+                return outputResponse;
+            }
         }
-        // GET: api/SalesForce/GetLeadToken?ValidationKey= ffe06298-22a8-4849-a46c-0284b04f2561
-      
-        /// <summary>
-        /// The function will post chat messages to lead description
-        /// </summary>
-        /// <param name="lData">An object data type converted to jason format</param>
-        /// <returns></returns>
-        
 
     }
-  
+    public class MyAppsDb
+    {
+        public static void GetRedirectURLParameters(ref string sf_authoize_url, ref string sf_clientid, ref string sf_callback_url)
+        {
+            string connStr = "server=dev-rds.cnhwwuo7wmxs.us-west-2.rds.amazonaws.com;user=root;database=apps;port=3306;password=a2387ass;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                conn.Open();
+                string sql = "SELECT * FROM integrations_constants where id = 1";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                if (rdr.HasRows)
+                {
+                    while (rdr.Read())
+                    {
+                        sf_authoize_url = rdr["sf_authoize_url"].ToString();
+                        sf_clientid = rdr["sf_clientid"].ToString();
+                        sf_callback_url = rdr["sf_callback_url"].ToString();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+            conn.Close();
+        }
+
+        public static void GetTokenParameters(ref string sf_clientid, ref string sf_callback_url, ref string sf_consumer_key, ref string sf_consumer_secret, ref string sf_token_req_end_point)
+        {
+            string connStr = "server=dev-rds.cnhwwuo7wmxs.us-west-2.rds.amazonaws.com;user=root;database=apps;port=3306;password=a2387ass;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                conn.Open();
+                string sql = "SELECT * FROM integrations_constants where id = 1";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                if (rdr.HasRows)
+                {
+                    while (rdr.Read())
+                    {
+                        sf_clientid = rdr["sf_clientid"].ToString();
+                        sf_callback_url = rdr["sf_callback_url"].ToString();
+                        sf_consumer_key = rdr["sf_consumer_key"].ToString();
+                        sf_consumer_secret = rdr["sf_consumer_secret"].ToString();
+                        sf_token_req_end_point = rdr["sf_token_req_end_point"].ToString();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+            conn.Close();
+        }
+
+        public static void CreateNewIntegrationSettingForUser(string username, string SFRefreshToken, string SFAccessToken, string SFApiVersion, string SFInstanceUrl)
+        {
+            string connStr = "server=dev-rds.cnhwwuo7wmxs.us-west-2.rds.amazonaws.com;user=root;database=apps;port=3306;password=a2387ass;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                conn.Open();
+                string sql = "insert into integration_settings(username, SFRefreshToken,SFRTCreationDT,SFAccessToken, SFApiVersion, SFInstanceUrl, SFATCreationDT)";
+                sql += " values ('" + username + "', '" + SFRefreshToken + "', now(), '"+ SFAccessToken + "', '" + SFApiVersion + "', '" + SFInstanceUrl + "', now())";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                int rows = cmd.ExecuteNonQuery();
+                
+            }
+            catch (Exception ex)
+            {
+            }
+            conn.Close();
+        }
+
+        public static void GetCurrentRefreshToken(string username, ref string SFRefreshToken)
+        {
+            string connStr = "server=dev-rds.cnhwwuo7wmxs.us-west-2.rds.amazonaws.com;user=root;database=apps;port=3306;password=a2387ass;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                conn.Open();
+                string sql = "SELECT SFRefreshToken FROM integration_settings where username = '" + username + "'";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                if (rdr.HasRows)
+                {
+                    while (rdr.Read())
+                    {
+                        SFRefreshToken = rdr["SFRefreshToken"].ToString();
+                    }
+                }
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+            conn.Close();
+        }
+
+        public static void UpdateIntegrationSettingForUser(string username, string SFAccessToken, string SFApiVersion, string SFInstanceUrl)
+        {
+            string connStr = "server=dev-rds.cnhwwuo7wmxs.us-west-2.rds.amazonaws.com;user=root;database=apps;port=3306;password=a2387ass;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                conn.Open();
+                string sql = "Update integration_settings Set SFAccessToken = ' " + SFAccessToken + "', SFApiVersion = ' " + SFApiVersion + "',SFInstanceUrl = ' " + SFInstanceUrl + "'";
+                sql += " WHERE username = '" + username + "'";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                int rows = cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+            }
+            conn.Close();
+        }
+
+
+    }
+
     public class SecureInfo
     {
         public string AccessToken { get; set; }
---
+++ obj/Release/Package/PackageTmp/App_Code/Db.cs
@@ -0,0 +1,317 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace SalesForceOAuth.App_Code
+{
+    
+    public class Db
+        {
+            //
+            private MySqlConnection connect;
+
+            //
+            private MySqlCommand command;
+
+            //
+            private MySqlDataReader reader;
+
+            //
+            private string connectiondetails = "server=dev-rds.cnhwwuo7wmxs.us-west-2.rds.amazonaws.com;user=root;database=apps;port=3306;password=a2387ass;";
+
+
+            public static void GetRedirectURLParameters(ref string sf_authoize_url, ref string sf_clientid,ref string sf_callback_url)
+            {
+                string connStr = "server=dev-rds.cnhwwuo7wmxs.us-west-2.rds.amazonaws.com;user=root;database=apps;port=3306;password=a2387ass;";
+                MySqlConnection conn = new MySqlConnection(connStr);
+                try
+                {
+                    conn.Open();
+                    string sql = "SELECT * FROM integrations_constants where id = 1";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    MySqlDataReader rdr = cmd.ExecuteReader();
+                    if (rdr.HasRows)
+                    {
+                        while (rdr.Read())
+                        {
+                            sf_authoize_url = rdr["sf_authoize_url"].ToString();
+                            sf_clientid = rdr["sf_clientid"].ToString();
+                            sf_callback_url = rdr["sf_callback_url"].ToString();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No rows found.");
+                    }
+                    rdr.Close();
+                }
+                catch (Exception ex)
+                {
+                }
+                conn.Close();
+        }
+
+            public static void GetTokenParameters(ref string sf_callback_url, ref string sf_consumer_key, ref string sf_consumer_secret, ref string sf_token_req_end_point)
+        {
+            string connStr = "server=dev-rds.cnhwwuo7wmxs.us-west-2.rds.amazonaws.com;user=root;database=apps;port=3306;password=a2387ass;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                conn.Open();
+                string sql = "SELECT * FROM integrations_constants where id = 1";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                if (rdr.HasRows)
+                {
+                    while (rdr.Read())
+                    {
+                        sf_callback_url = rdr["sf_callback_url"].ToString();
+                        sf_consumer_key = rdr["sf_consumer_key"].ToString();
+                        sf_consumer_secret = rdr["sf_consumer_secret"].ToString();
+                        sf_token_req_end_point = rdr["sf_token_req_end_point"].ToString();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+            conn.Close();
+        }
+
+        //
+        private bool bConnected = false;
+
+            //
+            private DataTable Table;
+
+            //
+            private int affected_rows;
+
+            //
+            private string squery;
+
+            //
+            private List<string> parameters;
+
+            public Db()
+            {
+                Connect();
+                Table = Table = new DataTable();
+                parameters = new List<string>();
+            }
+
+            private void Connect()
+            {
+                try
+                {
+                    MySqlConnection connection = new MySqlConnection(connectiondetails);
+                    connection.Open();
+                    connect = connection;
+                    bConnected = true;
+                }
+                catch (MySqlException ex)
+                {
+                    string exception = "Exception : " + ex.Message.ToString() + "\n\rApplication will close now. \n\r" + squery;
+                    Debug(exception);
+
+                    Environment.Exit(1);
+                }
+            }
+
+            public void CloseConn()
+            {
+                bConnected = false;
+                connect.Close();
+                connect.Dispose();
+            }
+
+            private void Init(string Query, string[] bindings = null)
+            {
+                // No connection with database? make connection
+                if (bConnected == false)
+                {
+                    Connect();
+                }
+
+                // Automatically disposes the MySQLcommand instance
+                using (command = new MySqlCommand(Query, connect))
+                {
+                    // 
+                    bind(bindings);
+
+                    // Prevents SQL injection
+                    if (parameters.Count > 0)
+                    {
+                        parameters.ForEach(delegate (string parameter)
+                        {
+                            string[] sparameters = parameter.ToString().Split('\x7F');
+                            command.Parameters.AddWithValue(sparameters[0], sparameters[1]);
+                        });
+                    }
+
+                    this.squery = Query.ToLower();
+
+                    if (squery.Contains("select"))
+                    {
+                        this.Table = execDatatable();
+                    }
+                    if (squery.Contains("delete") || squery.Contains("update") || squery.Contains("insert"))
+                    {
+                        this.affected_rows = execNonquery();
+                    }
+
+                    // Clear de parameters, 
+                    this.parameters.Clear();
+                }
+            }
+
+            public void bind(string field, string value)
+            {
+                parameters.Add("@" + field + "\x7F" + value);
+            }
+
+            public void bind(string[] fields)
+            {
+                if (fields != null)
+                {
+                    for (int i = 0; i < fields.Length; i++)
+                    {
+                        bind(fields[i], fields[i + 1]);
+                        i += 1;
+                    }
+                }
+            }
+
+            // Example:
+            // qBind(new string[] { "12", "John" });
+            // nQuery("SELECT * FROM User WHERE ID=@0 AND Name=@1");
+            /// <summary>
+            /// Bind multiple fields/values without identifier.
+            /// </summary>
+            /// <param name="fields"></param>
+            public void qBind(string[] fields)
+            {
+                if (fields != null)
+                {
+                    for (int i = 0; i < fields.Length; i++)
+                    {
+                        bind(i.ToString(), fields[i]);
+                    }
+                }
+            }
+
+            private DataTable execDatatable()
+            {
+                DataTable dt = new DataTable();
+                try
+                {
+                    reader = command.ExecuteReader();
+                    dt.Load(reader);
+                }
+                catch (MySqlException my)
+                {
+                    string exception = "Exception : " + my.Message.ToString() + "\n\r SQL Query : \n\r" + squery;
+
+                    //MessageBox.Show(exception, "Uncaught MYSQL Exception");
+
+                    Debug(exception);
+                }
+
+                return dt;
+            }
+
+            private int execNonquery()
+            {
+
+                int affected = 0;
+
+                try
+                {
+                    affected = command.ExecuteNonQuery();
+                }
+                catch (MySqlException my)
+                {
+                    string exception = "Exception : " + my.Message.ToString() + "\n\r SQL Query : \n\r" + squery;
+
+                    //MessageBox.Show(exception, "Uncaught MYSQL Exception");
+
+                    Debug(exception);
+                }
+
+                return affected;
+            }
+
+            public DataTable table(string table, string[] bindings = null)
+            {
+                Init("SELECT * FROM " + table, bindings);
+                return this.Table;
+            }
+
+            public List<object> table(string table, Type t)
+            {
+                return new List<object>();
+            }
+
+            public DataTable query(string query, string[] bindings = null)
+            {
+                Init(query, bindings);
+                return this.Table;
+            }
+
+            public int nQuery(string query, string[] bindings = null)
+            {
+                Init(query, bindings);
+                return this.affected_rows;
+            }
+
+            public string single(string query, string[] bindings = null)
+            {
+                Init(query, bindings);
+
+                if (Table.Rows.Count > 0)
+                {
+                    return Table.Rows[0][0].ToString();
+                }
+
+                return string.Empty;
+            }
+
+            public string[] row(string query, string[] bindings = null)
+            {
+                Init(query, bindings);
+
+                string[] row = new string[Table.Columns.Count];
+
+                if (Table.Rows.Count > 0)
+                    for (int i = 0; i++ < Table.Columns.Count; row[i - 1] = Table.Rows[0][i - 1].ToString()) ;
+
+                return row;
+            }
+
+            public List<string> column(string query, string[] bindings = null)
+            {
+                Init(query, bindings);
+
+                List<string> column = new List<string>();
+
+                for (int i = 0; i++ < Table.Rows.Count; column.Add(Table.Rows[i - 1][0].ToString())) ;
+
+                return column;
+            }
+
+
+            public void Debug(string error)
+            {
+                Console.WriteLine(error + "/n/r");
+            }
+        }
+   
+}
